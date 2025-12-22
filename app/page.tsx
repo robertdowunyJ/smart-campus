e61f40f7fc9b0553713041082d96897701e2786c
Youbin <youbin@Youbinui-MacBookPro.local>
2025-12-22 16:44:59 +0900

    p태그 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 24 }}>
       <h1>Smart Campus MVP</h1>
-      <p>Next.js is working ✅</p>
+      <p>Hello^^</p>
     </main>
   );
 }
